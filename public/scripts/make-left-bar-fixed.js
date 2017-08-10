6547ac438c7b19ec2ac9d5c7ec47353be12237ea
April Mintac Pineda <aprilmintacpineda@gmail.com>
2017-08-10 21:40:33 +0800

    homepage;mobile support;

--- public/scripts/make-left-bar-fixed.js
+++ public/scripts/make-left-bar-fixed.js
@@ -1,35 +1,57 @@
-// function makeFixed() {
-//   // cached
-//   var targetElement = document.querySelector('#make-fixed');
-//   var pageFooter = document.querySelector('.page-footer');
-//   var clientWidth = targetElement.clientWidth;
-  
-//   // check if pageYOffset is defined
-//   if(window.pageYOffset) {
-//     var offsetBottom = targetElement.offsetHeight + window.pageYOffset;
-//     var maxBottom = pageFooter.offsetTop - 80;
-//     // make the targetElement fixed or not fixed depending on the scroll bar
-//     if(window.pageYOffset >= 300) {
-//       targetElement.style.top = '80px';
-//       targetElement.style.background = '#fff';
-//       targetElement.style.zIndex = 999;
-//       targetElement.style.width = clientWidth + 'px';
+function canRun() {
+  return window.innerWidth >= 650;
+}
 
-//       if(offsetBottom >= maxBottom) {
-//         targetElement.style.position = 'absolute';
-//         targetElement.style.top = '0';
-//         document.body.onscroll = null;
-//       } else {
-//         targetElement.style.top = '80px';
-//         targetElement.style.position = 'fixed';
-//       }
-//     } else {
-//       targetElement.style.position = 'static';
-//     }
-//   }
-// }
+function resizeMakeFixed() {
+  if(canRun()) {
+    // cache
+    var leftBar = document.querySelector('.left-bar');
+    var targetElement = document.querySelector('#make-fixed');
+    targetElement.style.width = leftBar.clientWidth + 'px';
+  }
+}
 
-// document.body.onload = function() {
-//   makeFixed();
-//   document.body.onscroll = makeFixed;
-// }
+function makeFixed() {
+  // cached
+  var targetElement = document.querySelector('#make-fixed');
+  var leftBar = document.querySelector('.left-bar');
+  var rightBar = document.querySelector('.right-bar');
+  var pageFooter = document.querySelector('.page-footer');
+
+  if(canRun()) {
+    resizeMakeFixed();
+
+    var clientWidth = targetElement.clientWidth;
+
+    leftBar.style.height = leftBar.clientHeight > rightBar.clientHeight? leftBar.clientHeight + 'px' : rightBar.clientHeight + 'px';
+
+    // check if pageYOffset is defined
+    if(window.pageYOffset) {
+      var offsetBottom = targetElement.offsetHeight + window.pageYOffset;
+      var maxBottom = pageFooter.offsetTop - 80;
+      // make the targetElement fixed or not fixed depending on the scroll bar
+      if(window.pageYOffset >= 300) {
+        if(offsetBottom >= maxBottom) {
+          targetElement.style.position = 'absolute';
+          targetElement.style.bottom = '80px';
+          targetElement.style.top = 'auto';
+        } else {
+          targetElement.style.top = '80px';
+          targetElement.style.position = 'fixed';
+          targetElement.style.bottom = 'auto';
+        }
+      } else {
+        targetElement.style.position = 'static';
+      }
+    }
+  } else {
+    leftBar.style.height = 'auto';
+  }
+}
+
+document.body.onload = function() {
+  makeFixed();
+  resizeMakeFixed();
+  window.onresize = resizeMakeFixed;
+  document.body.onscroll = makeFixed;
+}
